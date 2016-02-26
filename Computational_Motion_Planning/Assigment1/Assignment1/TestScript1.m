9de417698aef6004eac4a79a68ceba2cf3adc33c
cbuscaron <cbuscaron@gmail.com>
2016-02-25 19:45:45 -0800

    Diksjtra's algorithm solution

---
+++ Computational_Motion_Planning/Assigment1/Assignment1/TestScript1.m
@@ -0,0 +1,28 @@
+%
+% TestScript for Assignment 1
+%
+
+%% Define a small map
+map = false(10*2)
+
+% Add an obstacle
+map (1:5*2, 6*2) = true;
+map (1:5*2, 6*2-1) = true;
+map (1:5*2, 6*2+1) = true;
+
+map (14:20, 6) = true;
+map (14:20, 6-1) = true;
+
+map (17:20, 12) = true;
+map (17:20, 12-1) = true;
+
+start_coords = [6*2, 2*2];
+dest_coords  = [8*2, 9*2];
+
+%map
+
+%%
+close all;
+[route, numExpanded] = DijkstraGrid (map, start_coords, dest_coords)
+% Uncomment following line to run Astar
+% [route, numExpanded] = AStarGrid (map, start_coords, dest_coords);
